9ce3485f05e5984700667585c3f5b604ca4fcef9
yangjun <290968698@qq.com>
2025-08-29 15:44:48 +0800

    add tray_manager

--- flutterclient/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutterclient/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import path_provider_foundation
 import screen_retriever
 import shared_preferences_foundation
 import sqflite_darwin
+import tray_manager
 import url_launcher_macos
 import window_manager
 
@@ -21,6 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
--- flutterclient/windows/flutter/generated_plugins.cmake
+++ flutterclient/windows/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
   screen_retriever
+  tray_manager
   url_launcher_windows
   window_manager
 )
